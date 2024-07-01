1dbebeea656971981dbbea700166beee8de19280
anikas2003 <anikas2003@gmail.com>
2024-06-30 17:55:22 -0700

    stacks

---
+++ testing.py
@@ -0,0 +1,18 @@
+# blah blah
+
+list1 = [2, 3, 5]
+list2 = [4, 6, 7]
+
+list3 = list1 + list2
+print(list3)
+list3.sort()
+
+# print(type(list3))
+# print(type(list2))
+# sorted_list = list3.sort()
+
+
+print(list3)
+# print(sorted_list)
+
+print(3/2)
